6f5e984fbb1eb5e49b9d7338450ca41bf5df3e07
Mohd Umar <mohdumar@Mohds-MacBook-Pro.local>
2019-05-14 13:42:12 +0800

    Initial Commit

--- providers.tf
+++ providers.tf
@@ -1,10 +1,3 @@
-/*provider "aws" {
-  region                  = "us-east-1"
-  shared_credentials_file = "root/project/.aws/credentials"
-  profile                 = "dev-s3"
-}
-*/
-  
-provider "aws" {
+  provider "aws" {
   region                  = "ap-east-1"
 }
